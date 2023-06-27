9815f478ce074abdc6e00616b6e3fb419e01913c
Savinov <savinov_vu@mail.ru>
2023-06-27 18:30:57 +0800

    119-122 JPA DSL(domain specific language) and @Query annotation

--- src/main/java/ru/savinov/pizzaservice/repositories/OrderRepository.java
+++ src/main/java/ru/savinov/pizzaservice/repositories/OrderRepository.java
@@ -1,7 +1,24 @@
 package ru.savinov.pizzaservice.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.savinov.pizzaservice.entities.PizzaOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<PizzaOrder, Long> {
+
+    List<PizzaOrder> findByDeliveryZip(String deliveryZip);
+
+    List<PizzaOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<PizzaOrder> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
+
+    List<PizzaOrder> findByDeliveryCityOrderByDeliveryTo(String city);
+
+    @Query("select o from PizzaOrder o where o.deliveryCity = :city")
+    List<PizzaOrder> readOrdersDeliveredInSeattle(@Param("city") String city);
+
 }
