d38ac991cb4c7c0174aef7255033ffecef62a6e9
Savinov <savinov_vu@mail.ru>
2023-07-26 21:59:09 +0800

    169-172 @AuthenticationPrincipal User user

--- src/main/java/ru/savinov/pizzaservice/controllers/OrderController.java
+++ src/main/java/ru/savinov/pizzaservice/controllers/OrderController.java
@@ -2,6 +2,7 @@ package ru.savinov.pizzaservice.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import ru.savinov.pizzaservice.entities.PizzaOrder;
+import ru.savinov.pizzaservice.entities.User;
 import ru.savinov.pizzaservice.repositories.OrderRepository;
 
 import javax.validation.Valid;
@@ -29,10 +31,14 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid PizzaOrder order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid PizzaOrder order,
+                               Errors errors,
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        log.info(user.getFullname());
         orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
