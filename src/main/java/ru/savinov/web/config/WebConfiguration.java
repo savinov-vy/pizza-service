b111fc9133c69c245ac8f7d2fac0275cf381622c
Savinov <savinov_vu@mail.ru>
2023-09-04 23:29:19 +0800

    profile !qa for WebConfiguration

--- src/main/java/ru/savinov/web/config/WebConfiguration.java
+++ src/main/java/ru/savinov/web/config/WebConfiguration.java
@@ -1,7 +1,9 @@
 package ru.savinov.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!qa")
 @Configuration
 public class WebConfiguration {
 }
