464eb4c4a91129d84fb41df083c098a54c46ce3b
Savinov <savinov_vu@mail.ru>
2023-09-19 22:59:47 +0800

    create liquibase scripts

--- src/main/java/ru/savinov/pizzaservice/entities/IngredientToPizza.java
+++ src/main/java/ru/savinov/pizzaservice/entities/IngredientToPizza.java
@@ -5,29 +5,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Data
 @Builder
-@Table(name = "ingredient_pizza")
+@Table(name = "ingredient_to_pizza")
 @AllArgsConstructor
 @NoArgsConstructor
-public class IngredientToPizza {
+public class IngredientToPizza implements BaseEntity<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "ingredient_id")
-    private String ingredientId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredient;
 
-    @Column(name = "pizza_id")
-    private Integer pizzaId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pizza_id")
+    private Pizza pizza;
 
 }
--- src/main/java/ru/savinov/pizzaservice/entities/Pizza.java
+++ src/main/java/ru/savinov/pizzaservice/entities/Pizza.java
@@ -7,16 +7,20 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import ru.savinov.pizzaservice.controllers.dto.PizzaDto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -35,16 +39,20 @@ public class Pizza implements BaseEntity<Long> {
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
+    @Column(name = "name")
     private String name;
 
-    @ManyToMany
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "ingredient_to_pizza", joinColumns = @JoinColumn(name = "pizza_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "ingredient_id", referencedColumnName = "id"))
+    private List<Ingredient> ingredients = new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "pizza_order_id")
     PizzaOrder pizzaOrder;
 
+    @Column(name = "created_at")
     private Date createdAt = new Date();
 
     public static Pizza ofDto(PizzaDto dto) {
