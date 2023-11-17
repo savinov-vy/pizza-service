1517f1ddd22ac367a4c37562ee9d34b0de67dac4
Savinov <savinov_vu@mail.ru>
2023-11-17 14:29:03 +0800

    add mock user to integration test

--- src/test/java/ru/savinov/pizzaservice/integration/annotation/IT.java
+++ src/test/java/ru/savinov/pizzaservice/integration/annotation/IT.java
@@ -1,6 +1,7 @@
 package ru.savinov.pizzaservice.integration.annotation;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.ElementType;
@@ -11,6 +12,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(value = RetentionPolicy.RUNTIME)
 @ActiveProfiles("test")
+@WithMockUser(username = "testUser", authorities = {"ADMIN", "USER"})
 @SpringBootTest
 public @interface IT {
 }
